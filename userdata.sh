7832681bd70856445ab78e9b2f096c167199facf
ErkanBarann <115561897+ErkanBarann@users.noreply.github.com>
2025-03-07 20:25:53 +0100

    Add basic blog layout and content

--- userdata.sh
+++ userdata.sh
@@ -3,13 +3,13 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
-git clone https://github.com/ErkanBarann/Aws-Final-Project.git
-cd /home/ubuntu/Aws-Final-Project/BlogDjango
+git clone https://github.com/ErkanBarann/aws-blog-architecture.git
+cd /home/ubuntu/aws-blog-architecture/BlogDjango
 apt install python3-pip -y
 apt-get install python3.8-dev default-libmysqlclient-dev -y
 apt-get install libjpeg-dev zlib1g-dev -y -qq
 pip3 install -r requirements.txt
-cd /home/ubuntu/Aws-Final-Project/BlogDjango/src
+cd /home/ubuntu/aws-blog-architecture/BlogDjango/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
